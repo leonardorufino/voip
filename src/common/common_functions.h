c8cac96c388d421a1e41632659fd2563caf924ed
Leonardo Maccari Rufino <eulmr1@gmail.com>
2017-06-26 23:15:36 -0300

    Added common functions with delay method

---
+++ src/common/common_functions.h
@@ -0,0 +1,24 @@
+/*
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * Written by Leonardo Maccari Rufino
+ */
+
+#pragma once
+
+#ifdef WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+class Common_Functions
+{
+public:
+    static void delay(unsigned int time);
+};
