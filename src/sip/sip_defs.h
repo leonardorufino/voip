9dd9a75d57cc823f699f2293cc910ba99f2149ea
Leonardo Maccari Rufino <eulmr1@gmail.com>
2017-09-24 16:14:39 -0300

    Created SIP object id class

--- src/sip/sip_defs.h
+++ src/sip/sip_defs.h
@@ -11,6 +11,9 @@
 
 #pragma once
 
+#include "util/util_defs.h"
+#include <string>
+
 #define SIP_VERSION     "SIP/2.0"
 
 enum SIP_Method_Type
@@ -98,4 +101,70 @@ enum SIP_Transport_Type
     SIP_TRANSPORT_WSS,
     SIP_TRANSPORT_SCTP,
     SIP_TRANSPORT_INVALID
+};
+
+class SIP_Object_ID
+{
+public:
+    static const unsigned int INVALID_USER_AGENT = INVALID_UNSIGNED_INT;
+    static const unsigned int INVALID_CALL = INVALID_UNSIGNED_INT;
+    static const unsigned int INVALID_DIALOG = INVALID_UNSIGNED_INT;
+    static const unsigned int INVALID_TRANSACTION = INVALID_UNSIGNED_INT;
+    static const unsigned int INVALID_TRANSPORT = INVALID_UNSIGNED_INT;
+
+public:
+    SIP_Object_ID() : _user_agent(INVALID_USER_AGENT), _call(INVALID_CALL), _dialog(INVALID_DIALOG),
+        _transaction(INVALID_TRANSACTION), _transport(INVALID_TRANSPORT) {}
+
+    std::string to_string()
+    {
+        std::string str;
+
+        if (_user_agent != INVALID_USER_AGENT)
+        {
+            str += "user_agent=";
+            str += std::to_string(_user_agent);
+        }
+
+        if (_call != INVALID_CALL)
+        {
+            if (!str.empty())
+                str += ", ";
+            str += "call=";
+            str += std::to_string(_call);
+        }
+
+        if (_dialog != INVALID_DIALOG)
+        {
+            if (!str.empty())
+                str += ", ";
+            str += "dialog=";
+            str += std::to_string(_dialog);
+        }
+
+        if (_transaction != INVALID_TRANSACTION)
+        {
+            if (!str.empty())
+                str += ", ";
+            str += "transaction=";
+            str += std::to_string(_transaction);
+        }
+
+        if (_transport != INVALID_TRANSPORT)
+        {
+            if (!str.empty())
+                str += ", ";
+            str += "transport=";
+            str += std::to_string(_transport);
+        }
+
+        return str;
+    }
+
+public:
+    unsigned int _user_agent;
+    unsigned int _call;
+    unsigned int _dialog;
+    unsigned int _transaction;
+    unsigned int _transport;
 };
