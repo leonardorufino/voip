4e6e588c964b14548238bc9252ed9917ede8de32
Leonardo Maccari Rufino <eulmr1@gmail.com>
2017-10-03 00:23:42 -0300

    Created SIP user agent call reject test

--- test/sip/sip_user_agent_test.h
+++ test/sip/sip_user_agent_test.h
@@ -95,3 +95,15 @@ public:
 };
 
 //-------------------------------------------
+//-------------------------------------------
+
+class SIP_User_Agent_Call_Reject_Test : public SIP_User_Agent_Test
+{
+public:
+    SIP_User_Agent_Call_Reject_Test() {}
+    virtual ~SIP_User_Agent_Call_Reject_Test() {}
+
+    bool run(Socket::Address_Family family, std::string address, unsigned short port, SIP_Transport_Type transport);
+};
+
+//-------------------------------------------
