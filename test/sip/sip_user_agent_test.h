d56bc987fc69f1c9e21e1349a8e4b71cb74491e2
Leonardo Maccari Rufino <eulmr1@gmail.com>
2017-10-18 21:57:38 -0200

    Created SIP user agent call cancel test

--- test/sip/sip_user_agent_test.h
+++ test/sip/sip_user_agent_test.h
@@ -164,3 +164,15 @@ public:
 };
 
 //-------------------------------------------
+//-------------------------------------------
+
+class SIP_User_Agent_Call_Cancel_Test : public SIP_User_Agent_Test
+{
+public:
+    SIP_User_Agent_Call_Cancel_Test() {}
+    virtual ~SIP_User_Agent_Call_Cancel_Test() {}
+
+    bool run(Socket::Address_Family family, std::string address, unsigned short port, SIP_Transport_Type transport);
+};
+
+//-------------------------------------------
