d611832397494e78080df42b7b33a2b68d68e128
Leonardo Maccari Rufino <eulmr1@gmail.com>
2017-10-25 21:32:34 -0200

    Created SIP user agent register reject test

--- test/sip/sip_user_agent_test.h
+++ test/sip/sip_user_agent_test.h
@@ -224,3 +224,15 @@ public:
 };
 
 //-------------------------------------------
+//-------------------------------------------
+
+class SIP_User_Agent_Register_Reject_Test : public SIP_User_Agent_Test
+{
+public:
+    SIP_User_Agent_Register_Reject_Test() {}
+    virtual ~SIP_User_Agent_Register_Reject_Test() {}
+
+    bool run(Socket::Address_Family family, std::string address, unsigned short port, SIP_Transport_Type transport);
+};
+
+//-------------------------------------------
