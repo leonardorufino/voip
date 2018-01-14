59ed3240660d8447dc8fe8082297c03c5946485e
Leonardo Maccari Rufino <eulmr1@gmail.com>
2018-01-14 19:04:43 -0200

    Created SIP body unknown class

--- src/sip/sip_body.cc
+++ src/sip/sip_body.cc
@@ -13,24 +13,6 @@
 
 //-------------------------------------------
 
-SIP_Body::SIP_Body(const SIP_Body &body)
-{
-    _size = body._size;
-    _body = new char[_size + 1];
-    memcpy(_body, body._body, _size);
-    _body[_size] = 0;
-}
-
-//-------------------------------------------
-
-SIP_Body::~SIP_Body()
-{
-    if (_body)
-        delete _body;
-}
-
-//-------------------------------------------
-
 SIP_Body *SIP_Body::create_body(SIP_Body_Type body_type, const SIP_Body *copy)
 {
     SIP_Body *body = NULL;
@@ -38,8 +20,8 @@ SIP_Body *SIP_Body::create_body(SIP_Body_Type body_type, const SIP_Body *copy)
     switch (body_type)
     {
         case SIP_BODY_UNKNOWN:
-            body = (!copy) ? new SIP_Body()
-                           : new SIP_Body(dynamic_cast<const SIP_Body &>(*copy));
+            body = (!copy) ? new SIP_Body_Unknown()
+                           : new SIP_Body_Unknown(dynamic_cast<const SIP_Body_Unknown &>(*copy));
             break;
         default:
             break;
@@ -49,8 +31,27 @@ SIP_Body *SIP_Body::create_body(SIP_Body_Type body_type, const SIP_Body *copy)
 }
 
 //-------------------------------------------
+//-------------------------------------------
 
-bool SIP_Body::decode(const char *body, unsigned short size)
+SIP_Body_Unknown::SIP_Body_Unknown(const SIP_Body_Unknown &body)
+{
+    _size = body._size;
+    _body = new char[_size + 1];
+    memcpy(_body, body._body, _size);
+    _body[_size] = 0;
+}
+
+//-------------------------------------------
+
+SIP_Body_Unknown::~SIP_Body_Unknown()
+{
+    if (_body)
+        delete _body;
+}
+
+//-------------------------------------------
+
+bool SIP_Body_Unknown::decode(const char *body, unsigned short size)
 {
     if ((!body) || (size == 0))
         return false;
@@ -64,7 +65,7 @@ bool SIP_Body::decode(const char *body, unsigned short size)
 
 //-------------------------------------------
 
-bool SIP_Body::encode(char *body, unsigned short &size)
+bool SIP_Body_Unknown::encode(char *body, unsigned short &size)
 {
     if ((!body) || (size <= _size))
         return false;
--- src/sip/sip_body.h
+++ src/sip/sip_body.h
@@ -16,16 +16,30 @@
 class SIP_Body
 {
 public:
-    SIP_Body() : _body(NULL), _size(0) {}
-    SIP_Body(const SIP_Body &body);
-    virtual ~SIP_Body();
+    SIP_Body() {}
+    virtual ~SIP_Body() {}
 
     static SIP_Body *create_body(SIP_Body_Type body_type, const SIP_Body *copy = NULL);
 
     // Virtual functions
-    virtual SIP_Body_Type get_body_type() { return SIP_BODY_UNKNOWN; }
-    virtual bool decode(const char *body, unsigned short size);
-    virtual bool encode(char *body, unsigned short &size);
+    virtual SIP_Body_Type get_body_type() = 0;
+    virtual bool decode(const char *body, unsigned short size) = 0;
+    virtual bool encode(char *body, unsigned short &size) = 0;
+};
+
+//-------------------------------------------
+
+class SIP_Body_Unknown : public SIP_Body
+{
+public:
+    SIP_Body_Unknown() : _body(NULL), _size(0) {}
+    SIP_Body_Unknown(const SIP_Body_Unknown &body);
+    virtual ~SIP_Body_Unknown();
+
+    // Virtual functions
+    SIP_Body_Type get_body_type() { return SIP_BODY_UNKNOWN; }
+    bool decode(const char *body, unsigned short size);
+    bool encode(char *body, unsigned short &size);
 
 protected:
     char *_body;
