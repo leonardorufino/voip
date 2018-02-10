513ba67e7c1ee202c4ed10ae5d25658e9841f62c
Leonardo Maccari Rufino <eulmr1@gmail.com>
2018-02-10 00:24:55 -0200

    Added log RTP transport source

--- src/util/log_manager.cc
+++ src/util/log_manager.cc
@@ -121,6 +121,7 @@ std::string Log_Manager::get_source(Log_Source source)
         case LOG_SDP_DESCRIPTION:   return "SDP DSC";
         case LOG_RTP_HEADER:        return "RTP HDR";
         case LOG_RTP_PACKET:        return "RTP PKT";
+        case LOG_RTP_TRANSPORT:     return "RTP TPT";
         default:                    return "       ";
     }
 }
