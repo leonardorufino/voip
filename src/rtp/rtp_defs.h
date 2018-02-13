9aebdb83c9647750b3fe47889b1e98cdda328bc3
Leonardo Maccari Rufino <eulmr1@gmail.com>
2018-02-13 21:54:20 -0200

    Added session in RTP object id

--- src/rtp/rtp_defs.h
+++ src/rtp/rtp_defs.h
@@ -23,17 +23,26 @@ enum RTP_Transport_Type
 class RTP_Object_ID
 {
 public:
+    static const unsigned int INVALID_SESSION = INVALID_UNSIGNED_INT;
     static const unsigned int INVALID_TRANSPORT = INVALID_UNSIGNED_INT;
 
 public:
-    RTP_Object_ID() : _transport(INVALID_TRANSPORT) {}
+    RTP_Object_ID() : _session(INVALID_SESSION), _transport(INVALID_TRANSPORT) {}
 
     std::string to_string()
     {
         std::string str;
 
+        if (_session != INVALID_SESSION)
+        {
+            str += "session=";
+            str += std::to_string(_session);
+        }
+
         if (_transport != INVALID_TRANSPORT)
         {
+            if (!str.empty())
+                str += ", ";
             str += "transport=";
             str += std::to_string(_transport);
         }
@@ -42,5 +51,6 @@ public:
     }
 
 public:
+    unsigned int _session;
     unsigned int _transport;
 };
