6c5998e26bba2aa122f8600eb1f28f416aa6b306
Leonardo Maccari Rufino <eulmr1@gmail.com>
2018-04-04 00:48:05 -0300

    Fixing Linux compilation

--- src/util/util_functions.h
+++ src/util/util_functions.h
@@ -30,6 +30,6 @@ public:
     static void delay(unsigned int time);
     static unsigned long get_tick();
     static int random();
-    static bool Util_Functions::HexToVector(std::string hex, std::vector<char> &vector);
-    static bool Util_Functions::VectorToHex(const std::vector<char> &vector, std::string &hex);
+    static bool HexToVector(std::string hex, std::vector<char> &vector);
+    static bool VectorToHex(const std::vector<char> &vector, std::string &hex);
 };
